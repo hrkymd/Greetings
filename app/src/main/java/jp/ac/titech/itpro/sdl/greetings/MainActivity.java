4bd08ecfc3f12a4e2c5dd4484906a12499f5aef1
Yamada <defdrhrk602@gmail.com>
2016-04-22 16:03:26 +0900

    String resourcelizatoin

--- app/src/main/java/jp/ac/titech/itpro/sdl/greetings/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/greetings/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private TextView outputView;
     private EditText inputName;
@@ -15,24 +15,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    outputView = (TextView) findViewById(R.id.output_view);
-    inputName = (EditText) findViewById(R.id.input_name);
-    okButton = (Button) findViewById(R.id.ok_button);
-    okButton.setOnClickListener(this);
+        outputView = (TextView) findViewById(R.id.output_view);
+        inputName = (EditText) findViewById(R.id.input_name);
     }
 
-    @Override
-    public void onClick (View v){
-    switch (v.getId()) {
-        case R.id.ok_button:
-            String name = inputName.getText().toString();
-            if (name != null && name.length() > 0)
-                outputView.setText("Hello," + name + "\nNice to see you!");
-            break;
+    //public メソッドなのでクリック以外でも呼び出すことができる
+    public void greet(View v){
+        String name = inputName.getText().toString();
+        if (name.length() > 0)
+            outputView.setText("Hello, " + name + ".\nNice to see you!");
         }
-    }
-
 }
